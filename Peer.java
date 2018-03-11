eda335eca1c902baf8e3bdadb2b4bbff82c6cc99
Abdul Hakim Shanavas <ahs7665@rit.edu>
2018-03-11 19:01:15 -0400

    1. Cleared few warnings 2. Fixed few bugs when node goes offline 3. Completed transferring of files when node goes offline 4. Completed display of files in the machine

--- Peer.java
+++ Peer.java
@@ -17,7 +17,7 @@ public class Peer implements Runnable {
 	private Socket nodeSocket;
 	private String host;
 	private int guid;
-	private boolean isListeningThread = false;
+	private boolean isOnline = false;
 	private FingerTable fingerTable;
 	private Thread listeningThread;
 	private TreeMap<Integer, InetAddress> liveNodes;
@@ -33,7 +33,6 @@ public class Peer implements Runnable {
 		files = new ArrayList<String>();
 		listeningThread = new Thread(this);
 		listeningThread.setDaemon(true);
-		this.isListeningThread = true;
 		listeningThread.start();
 	}
 
@@ -54,35 +53,59 @@ public class Peer implements Runnable {
 
 	public static void menu(Peer node) {
 		while (true) {
-			System.out.println("1. Join the network \n 2. Leave the network\n 3. Insert file\n 4. Show finger table");
+			System.out.println("1. Join the network \n 2. Leave the network\n 3. Insert file\n 4. Show finger table \n 5.Show files in this machine");
 			Scanner scan = new Scanner(System.in);
 			System.out.println("Welcome");
 			try {
 				switch (scan.nextInt()) {
 				case 1:
 					node.enterNetwork();
-					//node.constructFingerTable();
+					node.isOnline = true;
 					System.out.println("Joined the network");
 					break;
 				case 2:
-					node.leaveNetwork();
-					System.out.println("Left the network");
+					if(node.isOnline) {
+						node.leaveNetwork();
+						node.isOnline = false;
+						System.out.println("Left the network");
+						node.files.clear();
+					}else {
+						System.out.println("Node is already offline");
+					}
+					
 					break;
 				case 3:
-					System.out.println("Please enter file name");
-					String file = scan.next();
-					int idToBeInserted = Math.abs(file.hashCode() % MAX_NODES );
-					System.out.println("File id: " + idToBeInserted);
-					node.insertFileAtID(idToBeInserted, file);
+					if(node.isOnline) {
+						System.out.println("Please enter file name");
+						String file = scan.next();
+						int idToBeInserted = Math.abs(file.hashCode() % MAX_NODES );
+						System.out.println("File id: " + idToBeInserted);
+						node.insertFileAtID(idToBeInserted, file);
+					}else {
+						System.out.println("Node is offline! Must be online to insert file");
+					}
 					break;
 				case 4:
 					System.out.println("Printing finger table");
-					node.fingerTable.printFingerTable();
+					if(node.isOnline) {
+						node.fingerTable.printFingerTable();
+					}else {
+						System.err.println("Node is offline");
+					}
 					break;
+				case 5:
+					if(node.files.size() == 0) {
+						System.out.println("No files in this machine!");
+					}else {
+						System.out.println("There are " + node.files.size() + " files in this machine");
+						for (String fileName : node.files) {
+							System.out.println("\t\t" + fileName);
+						}
+					}
+					
 				}
 
 			} catch (InputMismatchException e) {
-				// TODO: handle exception
 				System.out.println("Invalid input");
 
 			}
@@ -96,10 +119,8 @@ public class Peer implements Runnable {
 			inputstream = new ObjectInputStream(nodeSocket.getInputStream());
 			outputStream = new ObjectOutputStream(nodeSocket.getOutputStream());
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -121,12 +142,8 @@ public class Peer implements Runnable {
 			connect();
 			outputStream.writeObject("Joining");
 			outputStream.writeObject(guid);
-			//liveNodes = (TreeMap<Integer, InetAddress>) inputstream.readObject();
 			System.out.println("GUID : " + guid);
-			//System.out.println("Size : " + liveNodes.size());
-			//outputStream.writeObject("Received livenodes");
 		}catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
 			closeConnection();
@@ -138,9 +155,11 @@ public class Peer implements Runnable {
 			connect();
 			outputStream.writeObject("Going offline");
 			outputStream.writeObject(guid);
+			int id = fingerTable.successorTable.firstKey();
+			int successor = fingerTable.successorTable.get(id);
+			transferFileToNode(successor, successor, "", false);
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
 			closeConnection();
@@ -160,7 +179,7 @@ public class Peer implements Runnable {
 			boolean isNodeAvailable = fingerTable.successorTable.containsKey(id);
 			if (isNodeAvailable) {
 				int successorID = fingerTable.successorTable.get(id);
-				transferFileToNode(successorID, successorID, file);
+				transferFileToNode(successorID, successorID, file, true);
 			}else {
 				int sourceID;
 				int minDistance = 20;
@@ -174,9 +193,9 @@ public class Peer implements Runnable {
 				int idToSend = nodesDistance.get(minDistance);
 				int successorId = fingerTable.successorTable.get(idToSend);
 				if(checkBetweenNodes(idToSend, successorId, id)) {
-					transferFileToNode(successorId, successorId, file);
+					transferFileToNode(successorId, successorId, file, true);
 				}else {
-					transferFileToNode(id, successorId, file);
+					transferFileToNode(id, successorId, file, true);
 				}
 				
 			}
@@ -211,15 +230,21 @@ public class Peer implements Runnable {
 		return distance;
 	}
 	
-	public void transferFileToNode(int idToBeInserted, int successorID, String file) {
+	public void transferFileToNode(int idToBeInserted, int successorID, String file, boolean isFile) {
 		try {
 			InetAddress successorIP = liveNodes.get(successorID);
 			Socket socket = new Socket(successorIP, LISTENING_PORT);
 			ObjectOutputStream opStream = new ObjectOutputStream(socket.getOutputStream());
-			opStream.writeObject("Insert File");
-			System.out.println("File :" + file + " = " + idToBeInserted  + " routed to :" + successorID);
-			opStream.writeObject(idToBeInserted);
-			opStream.writeObject(file);
+			if(isFile) {
+				opStream.writeObject("Insert file");
+				System.out.println("File :" + file + " = " + idToBeInserted  + " routed to :" + successorID);
+				opStream.writeObject(idToBeInserted);
+				opStream.writeObject(file);
+			}else {
+				opStream.writeObject("Transferring files");
+				opStream.writeObject(files);
+			}
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -228,6 +253,7 @@ public class Peer implements Runnable {
 		}
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -242,7 +268,7 @@ public class Peer implements Runnable {
 					System.out.println("Received updated livenodes");
 					constructFingerTable();
 					break;
-				case "Insert File" :
+				case "Insert file" :
 					int id = (int)inputStream.readObject();
 					String file = (String)inputStream.readObject();
 					System.out.println("Insert file at :" + id);
@@ -250,6 +276,13 @@ public class Peer implements Runnable {
 					insertFileAtID(id, file);
 					break;
 				case "Find File" :
+					break;
+				case "Transferring files" :
+					ArrayList<String> tempFiles = (ArrayList<String>)inputStream.readObject();
+					for (String string : tempFiles) {
+						files.add(string);
+					}
+					System.out.println("Files received from predecessor");
 					break;
 				}
 			}
@@ -267,21 +300,4 @@ public class Peer implements Runnable {
 
 	}
 
-//	public void listenerForUpdatingFingerTable() {
-//		this.isListeningThread = false;
-//		while (true) {
-//			try {
-//				ServerSocket listeningSocket = new ServerSocket(LISTENING_PORT);
-//				Socket socket = listeningSocket.accept();
-//				ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-//				// ObjectOutputStream outputStream = new
-//				// ObjectOutputStream(socket.getOutputStream());
-//				liveNodes = (TreeMap<Integer, InetAddress>) inputStream.readObject();
-//				System.out.println("Received updated livenodes");
-//				new Thread(this).start();
-//			} catch (Exception e) {
-//				// TODO: handle exception
-//			}
-//		}
-//	}
 }
